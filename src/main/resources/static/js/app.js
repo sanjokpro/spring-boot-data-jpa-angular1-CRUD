487478f902bd981f5608a7b864618a30fb36cea0
Sanjok Khakurel <sanjokkhakurel@gmail.com>
2017-09-12 15:05:06 +0545

    ubi modal near completion

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,3 @@
 angular.module("hrmsLogin", ['restangular', 'ngStorage']);
 
-angular.module("hrmsDashboard", ['restangular', 'ngStorage', 'ui.router', 'ngTable']);
+angular.module("hrmsDashboard", ['restangular', 'ngStorage', 'ui.router', 'ngTable', 'ui.bootstrap', 'ngAnimate']);
